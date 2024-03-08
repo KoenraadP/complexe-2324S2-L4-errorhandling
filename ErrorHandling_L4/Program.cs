8a7be56df4a308ea27a2224f4aafe9e8c4543f26
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-03-08 10:06:10 +0100

    Add more code to error handling example

--- ErrorHandling_L4/Program.cs
+++ ErrorHandling_L4/Program.cs
@@ -11,19 +11,50 @@ namespace ErrorHandling_L4
         static void Main(string[] args)
         {
             // welke code wil je 'proberen' uit te voeren
-            try
+            /*try
             {
                 string[] months = new string[12];
                 months[12] = "December";
             }
             // wat moet er gebeuren als het niet lukt om de code
             // in het try blokje uit te voeren
-            catch
+            catch (Exception e) 
             {
-                Console.WriteLine("Whoops");
-            }
+                //Console.WriteLine(e);
+                Console.WriteLine(e.Message);
+            }*/
 
-            
+            try
+            {
+                string[] months = new string[12];
+                //months[12] = "December";
+                months[0] = "Januari";
+                int number = Convert.ToInt32(months[0]);
+            }
+            // specifiek fout afhandelen voor index
+            // buiten de grenzen van de array
+            catch (IndexOutOfRangeException e) 
+            {
+                Console.WriteLine("Arrays beginnen vanaf nul!");
+            }
+            // conversie error opvangen
+            catch (FormatException e)
+            {
+                Console.WriteLine("Je probeert een onmogelijke conversie te doen!");
+            }
+            // andere fouten opvangen
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("Er is iets fout gelopen: " +
+                    e.Message); 
+            }
+            // code die altijd uitgevoerd wordt, of de try
+            // nu goed of slecht afgelopen is
+            finally
+            {
+                Console.WriteLine("Dit zie je altijd");
+            }
         }
     }
 }
